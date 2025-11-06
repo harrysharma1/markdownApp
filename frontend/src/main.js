360a85cd527b0faf2dbcf37b9fff8a4f5dbdacbd
Harry Sharma <harrysharma1066@gmail.com>
2025-11-06 23:02:22 +0000

    update: - add multiple markdown-it modules to expand markdown parser - change tabs to be two spaces - TODO: find reason for h5 and h6 not rendering

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,15 @@
 import {BrowserOpenURL} from "../wailsjs/runtime/runtime.js";
 import markdownit from "markdown-it";
+import markdownItFootnote from "markdown-it-footnote";
+import markdownitSup from "markdown-it-sup";
+import markdownitSub from "markdown-it-sub";
+import markdownitMark from "markdown-it-mark"
+import markdownitDeflist from "markdown-it-deflist";
+import markdownitIns from "markdown-it-ins";
+import markdownitAbbr from "markdown-it-abbr";
+import {full as emoji} from "markdown-it-emoji";
 import hljs from "highlight.js"
-import "highlight.js/styles/atom-one-dark.css";
+import "highlight.js/styles/github.css";
 
 // Set up global variables
 const md = markdownit(
@@ -9,6 +17,7 @@ const md = markdownit(
         html: true,
         linkify: true,
         typographer: true,
+        breaks: true,
         highlight:  function(str, lang){
             if (lang && hljs.getLanguage(lang)){
                 try{
@@ -25,7 +34,15 @@ const md = markdownit(
                 "</code></pre>"
             );
         },
-    });
+    })
+    .use(markdownItFootnote)
+    .use(emoji)
+    .use(markdownitSup)
+    .use(markdownitSub)
+    .use(markdownitMark)
+    .use(markdownitIns)
+    .use(markdownitDeflist)
+    .use(markdownitAbbr);
 const plainText = document.getElementById("plainText");
 const mdText = document.getElementById("mdText");
 
@@ -44,7 +61,7 @@ plainText.addEventListener("keydown", (event) =>{
         
         const selection = window.getSelection();
         const range = selection.getRangeAt(0);
-        const tabNode = document.createTextNode("    ");
+        const tabNode = document.createTextNode("  ");
         range.insertNode(tabNode);
         range.setStartAfter(tabNode);
         range.setEndAfter(tabNode);
@@ -53,9 +70,27 @@ plainText.addEventListener("keydown", (event) =>{
     }
 });
 
-// Conversion
+// Auto Conversion
+function debounce(fn, delay=200){
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(()=>fn(...args), delay);
+    };
+}
+
+const autoConvert = debounce(()=>{
+    const htmlOutput = md.render(plainText.innerText);
+    console.log(htmlOutput);
+    mdText.innerHTML = htmlOutput;
+}, 200);
+
+plainText.addEventListener("input", autoConvert);
+
+// Manual Conversion
 function convertToMd(){
-    const htmlOutput = md.render(plainText.innerHTML);
+    const htmlOutput = md.render(plainText.innerText);
+    console.log(htmlOutput);
     mdText.innerHTML = htmlOutput;
 }
 document.getElementById("convertBtn").addEventListener("click", convertToMd);
