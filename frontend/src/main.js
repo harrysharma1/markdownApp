328f4ca8a539cfbd22268af5b0b03e5b6a9dd999
Harry Sharma <harrysharma1066@gmail.com>
2025-11-06 18:43:25 +0000

    feat: - Add js for highlighting and markdown rendering - Add html frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,43 +1,61 @@
-import './style.css';
-import './app.css';
+import {BrowserOpenURL} from "../wailsjs/runtime/runtime.js";
+import markdownit from "markdown-it";
+import hljs from "highlight.js"
+import "highlight.js/styles/atom-one-dark.css";
 
-import logo from './assets/images/logo-universal.png';
-import {Greet} from '../wailsjs/go/main/App';
+// Set up global variables
+const md = markdownit(
+    {
+        html: true,
+        linkify: true,
+        typographer: true,
+        highlight:  function(str, lang){
+            if (lang && hljs.getLanguage(lang)){
+                try{
+                    return(
+                        '<pre class="hljs"><code>' + 
+                        hljs.highlight(str, {language:lang, ignoreIllegals:true}).value + 
+                        "</code></pre>"
+                    );
+                }catch (_){}
+            }
+            return (
+                '<pre class="hljs"><code>' +
+                md.utils.escapeHtml(str) +
+                "</code></pre>"
+            );
+        },
+    });
+const plainText = document.getElementById("plainText");
+const mdText = document.getElementById("mdText");
 
-document.querySelector('#app').innerHTML = `
-    <img id="logo" class="logo">
-      <div class="result" id="result">Please enter your name below 👇</div>
-      <div class="input-box" id="input">
-        <input class="input" id="name" type="text" autocomplete="off" />
-        <button class="btn" onclick="greet()">Greet</button>
-      </div>
-    </div>
-`;
-document.getElementById('logo').src = logo;
-
-let nameElement = document.getElementById("name");
-nameElement.focus();
-let resultElement = document.getElementById("result");
-
-// Setup the greet function
-window.greet = function () {
-    // Get name
-    let name = nameElement.value;
-
-    // Check if the input is empty
-    if (name === "") return;
-
-    // Call App.Greet(name)
-    try {
-        Greet(name)
-            .then((result) => {
-                // Update result with data back from App.Greet()
-                resultElement.innerText = result;
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    } catch (err) {
-        console.error(err);
+// Event listeners
+document.addEventListener("click", (event)=>{
+    const target = event.target.closest("a");
+    if(target && target.href.startsWith("http")){
+        event.preventDefault();
+        BrowserOpenURL(target.href);
     }
-};
+});
+
+plainText.addEventListener("keydown", (event) =>{
+    if(event.key === "Tab"){
+        event.preventDefault();
+        
+        const selection = window.getSelection();
+        const range = selection.getRangeAt(0);
+        const tabNode = document.createTextNode("    ");
+        range.insertNode(tabNode);
+        range.setStartAfter(tabNode);
+        range.setEndAfter(tabNode);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+});
+
+// Conversion
+function convertToMd(){
+    const htmlOutput = md.render(plainText.innerHTML);
+    mdText.innerHTML = htmlOutput;
+}
+document.getElementById("convertBtn").addEventListener("click", convertToMd);
