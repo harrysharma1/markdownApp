12c5546ba564a4332991442bedc6a605fb932cf7
Harry Sharma <harrysharma1066@gmail.com>
2025-11-06 18:25:59 +0000

    feat: set up tailwind styling, highlightjs and markdown-it

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",            
+    "./src/**/*.{js,html,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+}
